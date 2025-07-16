aea2142d6c5a6e4ecd381529a98a2bcaf5f4ade5
NyxArsene <nyaple3@gmail.com>
2025-07-16 19:29:49 -0400

    hi

--- send_pose.py
+++ send_pose.py
@@ -1,10 +1,10 @@
 import serial, struct, time
 
-COM  = "COM3"          # check Device Manager
+COM  = "COM4"          # check Device Manager
 BAUD = 2_000_000       # same as Pi script
 pack = struct.Struct(">H6B")          # duration + 6 angles
 
-pose = [90, 90, 90, 90, 90, 90]       # any six ints
+pose = [90, 90, 60, 90, 90, 90]       # any six ints
 dur  = 500                            # half-second move
 
 with serial.Serial(COM, BAUD, timeout=1) as ser:
